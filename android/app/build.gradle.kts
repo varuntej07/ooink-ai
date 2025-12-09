de6e17ba55fb33720b147bf764e64278215d391a
varuntej07 <varuntej07.cv@gmail.com>
2025-12-08 21:39:54 -0800

    Updated the bundle identifier to `com.ooinkramen.ai` for both Android and iOS and configured the Codemagic CI/CD workflow for TestFlight releases.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 android {
-    namespace = "com.ooinkramen.ooink"
+    namespace = "com.ooinkramen.ai"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -25,7 +25,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.ooinkramen.ooink"
+        applicationId = "com.ooinkramen.ai"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/ooinkramen/ai/MainActivity.kt
+++ android/app/src/main/kotlin/com/ooinkramen/ai/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ooinkramen.ooink
+package com.ooinkramen.ai
 
 import io.flutter.embedding.android.FlutterActivity
 
